f4e13d277569756b7b3a6c31802961e18bd744f7
César Almeida <cesaralmeida89@gmail.com>
2023-03-08 20:02:17 +0100

    :wrench: add react-router-dom

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		publicPath: '/',
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
@@ -43,4 +44,7 @@ module.exports = {
 			filename: '[name].css',
 		}),
 	],
+	devServer: {
+		historyApiFallback: true,
+	},
 };
