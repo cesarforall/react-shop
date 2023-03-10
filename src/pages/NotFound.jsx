2d492b8c257a4da7c64c7b00c8bfa58a10ee4f85
César Almeida <cesaralmeida89@gmail.com>
2023-03-10 18:55:39 +0100

    :hammer: add full pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
+import React from 'react';
+
 const NotFound = () => {
-	return <div>No encontrado, error 404.</div>;
-};
+	return (
+		<div>
+			No encontrado, error 404.
+		</div>
+	);
+}
 
 export default NotFound;
