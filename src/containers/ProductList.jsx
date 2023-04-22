f04c9d32143c5ce9619edce29d099666e098f65f
Cesar Almeida <cesaralmeida89@gmail.com>
2023-04-22 21:04:51 +0200

    :hammer: add product details from data using props

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -10,7 +10,7 @@ const ProductList = () => {
 		<section className='main-container'>
 			<div className='ProductList'>
 				{products.map(product => (
-					<ProductItem />
+					<ProductItem product={product} key={product.id} />
 				))}
 			</div>
 		</section>
